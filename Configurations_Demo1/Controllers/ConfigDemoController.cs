a128ca601d9be87241cedcf98c6783855fdcafa0
Sreenesh Reddy <hizahero@gmail.com>
2025-10-22 17:53:35 +0530

    Add project files.

---
+++ Configurations_Demo1/Controllers/ConfigDemoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Configurations_Demo1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfigDemoController : ControllerBase
+    {
+        
+        protected readonly IConfiguration _config;
+
+        public ConfigDemoController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public IActionResult GetAppSettings()
+        {
+            var appName = _config["AppSettings:ApplicationName"];
+            var version = _config["AppSettings:Version"];
+            return Ok(new {appName, version});
+        }
+    }
+}
